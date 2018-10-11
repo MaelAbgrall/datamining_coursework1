71b3ac92759f52edba90da613ae1c6dc55e25c1c
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-10-11 14:21:46 +0100

    kneighbors and confusion matrix done

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import utils.filehandler as filehandler
+from sklearn.neighbors import NearestNeighbors
+
+import time
 
 #import the csv file
 dataset = filehandler.import_csv('../fer2018/fer2018.csv')
@@ -9,6 +12,12 @@ set_list = filehandler.split_dataset(10, dataset)
 #learn
 for position in range(10):
    (x_train, y_train, x_validation, y_validation) = set_list[position]
-   
    #call learn using x_train, x_validation, etc
+   
 
+start_time = time.time()
+
+nbrs = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(x_train[0:1000])
+_, indices = nbrs.kneighbors(x_train)
+
+print(time.time() - start_time)
