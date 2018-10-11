9d5d378e5559368d99793985bb22f437642411a9
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-10-11 14:56:53 +0100

    kneighbors for fer2018.csv done

--- src/main.py
+++ src/main.py
@@ -1,23 +1,91 @@
 import utils.filehandler as filehandler
-from sklearn.neighbors import NearestNeighbors
-
+from sklearn.neighbors import KNeighborsClassifier
 import time
+
+# for confusion matrix
+import pandas as pd
+import seaborn as sn
+from sklearn.metrics import confusion_matrix
 
 #import the csv file
 dataset = filehandler.import_csv('../fer2018/fer2018.csv')
 
+
 #prepare 10 subsets for cross validation
 set_list = filehandler.split_dataset(10, dataset)
 
-#learn
 for position in range(10):
    (x_train, y_train, x_validation, y_validation) = set_list[position]
-   #call learn using x_train, x_validation, etc
-   
+
+
+#Classification: Performance of the Nearest Neighbour Algorithm on the given data set
+
+"""
+Classifier initialization
+"""
+knc = KNeighborsClassifier(algorithm='ball_tree', n_neighbors=3)
 
 start_time = time.time()
+knc.fit(x_train, y_train)
+print(time.time() - start_time, "seconds to fit data")
 
-nbrs = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(x_train[0:1000])
-_, indices = nbrs.kneighbors(x_train)
 
-print(time.time() - start_time)
+
+
+"""
+Running the algorithm. preds will hold the class predictions
+"""
+start_time = time.time()
+preds = knc.predict(x_validation)
+print(time.time() - start_time, "seconds to predict the classes")
+
+
+
+
+"""
+Overall accuracy calculation
+"""
+correct_count = (preds == y_validation).sum()
+print(correct_count)
+accuracy = correct_count / len(preds)
+print(accuracy)
+
+
+#Classification Accuracy Calculation
+
+
+
+
+"""
+Computing confusion matrix to evaluate classification accuracy
+"""
+
+confusion_matrix = confusion_matrix(y_validation, preds)
+
+
+
+"""
+Plotting the confusion matrix
+"""
+
+df_cm = pd.DataFrame(confusion_matrix, index = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'],
+                  columns = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'])
+plt.figure(figsize = (8, 6))
+sn.heatmap(df_cm, annot=True, fmt="d", cmap='Greys')
+
+
+
+"""
+Plotting the accuracy histogram
+"""
+acc = []
+for i in range(len(confusion_matrix)):
+    total_emote = confusion_matrix[i].sum()
+    acc.append(confusion_matrix[i][i] / total_emote)
+    
+bar_width = .5
+positions = np.arange(7)
+plt.bar(positions, acc, bar_width)
+plt.xticks(positions + bar_width / 2, ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'))
+plt.show()
+
